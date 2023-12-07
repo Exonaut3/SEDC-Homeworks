79c60dd8716b809fad7d5770a2ea3e6471d60f4e
Simeon Dvojakov <dvojakovsimeon@gmail.com>
2023-12-07 12:37:49 +0100

    AAAAAAAAAAAAAAAAAAAAAA

---
+++ Homework_10_phone_maths/script.js
@@ -0,0 +1,17 @@
+let phonePrice = 199.95;
+let taxRate = 0.05;
+let numberOfPhones = 30;
+let priceTimesPhones = phonePrice * numberOfPhones;
+let rezultat = priceTimesPhones + (priceTimesPhones * taxRate);
+
+console.log(rezultat + "$");
+
+console.log("-----------------------------------"); 
+
+function finalprice(phonePrice, taxRate, numberOfPhones){
+    let rezultat2 =  (phonePrice * numberOfPhones) + ((phonePrice * numberOfPhones)  * taxRate);
+    return rezultat2;
+}
+
+let rezultat2 = finalprice(199.95, 0.05, 30)
+console.log(rezultat2+"$");
